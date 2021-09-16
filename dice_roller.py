5d91cc5dac80d985df50d7f7681b54a522c08204
hcoyle91 <56696954+hcoyle91@users.noreply.github.com>
2021-09-16 10:40:10 -0400

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,16 +1,16 @@
-import random
+import random ## imports a library random
 
-print("Automatic Dice Roller")
+print("Automatic Dice Roller") ## prints the title 
 
-value="" 
+value="" ## initializes the variable value as " "
 
-while value == "":
-    randomNum = random.randint(1, 6) 
-    value = input("Press Enter to roll, press x then Enter to quit")
+while value == "":   # because value is already set as "" the while loop begins
+    randomNum = random.randint(1, 6) ## set randomnum to 
+    value = input("Press Enter to roll, press x then Enter to quit") ## takes an input
     
-    if value == "": 
+    if value == "":  ## if user hits enter, it will take the randomnum (already set to a random integer) and print it
         print(randomNum)
         print("Roll the dice again?")
     else:
-        print("Ok, Goodbye")
+        print("Ok, Goodbye") ## if the user types anything else, say good bye
     
