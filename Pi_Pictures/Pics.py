ec7eb5107426eb148e846e288f1337ce5b8f005c
hcoyle91 <56696954+hcoyle91@users.noreply.github.com>
2022-01-27 13:16:10 -0500

    Update Pics.py

--- Pi_Pictures/Pics.py
+++ Pi_Pictures/Pics.py
@@ -1,17 +1,17 @@
-import time
+import time  # imports time library
 import picamera
-effect_arr = ['negative', 'solarize', 'sketch', 'denoise', 'cartoon']
-name_arr = ['picture1.jpg', 'picture2.jpg', 'picture3.jpg', 'picture4.jpg', 'picture5.jpg']
+effect_arr = ['negative', 'solarize', 'sketch', 'denoise', 'cartoon']       #Creates two arrays one haveing the different effects  
+name_arr = ['picture1.jpg', 'picture2.jpg', 'picture3.jpg', 'picture4.jpg', 'picture5.jpg']     #the other having names to be assignned to the 5 pictures
 i = 0
 x = 0
 with picamera.PiCamera() as camera:
-    camera.resolution = (1024, 768)
+    camera.resolution = (1024, 768)     #Intitializes The Pi camera
     camera.start_preview()
     print("Running...")
-    for x in range(5):
+    for x in range(5):                  # A for loops to take five pics with different affects
         time.sleep(.1)
-        camera.image_effect = effect_arr[i]
+        camera.image_effect = effect_arr[i]     
         camera.capture(name_arr[x])
-        print("Another picture taken")
+        print("Another picture taken")          # Prints after the camera takes each picture
         i +=1
         x +=1
