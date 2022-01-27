baed53821cde6daecb2e4635d21581e32b156bc3
hcoyle91 <hcoyle91@charlottesvilleschools.org>
2022-01-27 14:39:30 +0000

    updating

--- Pi_Pictures/Pics.py
+++ Pi_Pictures/Pics.py
@@ -2,8 +2,10 @@ import time
 import picamera
 
 with picamera.PiCamera() as camera:
+    print("Running...")
     camera.resolution = (1024, 768)
     camera.start_preview()
     # Camera warm-up time
     time.sleep(2)
-    camera.capture('foo.jpg')
+    camera.capture('Picture2.jpg')
+    print("All done, picture taken")
