15ffd26ebfe0373de55b009ab96050a6070efaaa
hcoyle91 <56696954+hcoyle91@users.noreply.github.com>
2022-01-27 09:51:55 -0500

    Update Pics.py

--- Pi_Pictures/Pics.py
+++ Pi_Pictures/Pics.py
@@ -1,7 +1,9 @@
 import time
 import picamera
 effect_arr = ['negative', 'solarize', 'sketch', 'denoise', 'cartoon']
+name_arr = ['picture1.jpg', 'picture2.jpg', 'picture3.jpg', 'picture4.jpg', 'picture5.jpg']
 i = 0
+x = 0
 with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
     camera.start_preview()
@@ -9,6 +11,7 @@ with picamera.PiCamera() as camera:
     for x in range(5):
         time.sleep(.1)
         camera.image_effect = effect_arr[i]
-        camera.capture('Picture2.jpg')
+        camera.capture(name_arr[x])
         print("Another picture taken")
-        i = i+1
+        i +=1
+        x +=1
