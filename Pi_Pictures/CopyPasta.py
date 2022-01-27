067f81431354b84d59a5bc46587506facbc9f9f9
hcoyle91 <56696954+hcoyle91@users.noreply.github.com>
2022-01-27 13:38:06 -0500

    Create CopyPasta.py

---
+++ Pi_Pictures/CopyPasta.py
@@ -0,0 +1,17 @@
+from picamera import PiCamera
+from time import sleep
+from gpiozero import Button
+
+button = Button(17)
+camera = PiCamera()
+
+camera.start_preview()
+frame = 1
+while True:
+    try:
+        button.wait_for_press()
+        camera.capture('/home/pi/Documents/Engineering_4_Notebook/Pics.py/frame%03d.jpg' % frame)
+        frame += 1
+    except KeyboardInterrupt:
+        camera.stop_preview()
+        break
