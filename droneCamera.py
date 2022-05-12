e90b169d019cd09ff3fedd14d7555060a7e07462
hcoyle91 <hcoyle91@charlottesvilleschools.org>
2022-05-12 14:34:41 +0100

    testtttt

--- droneCamera.py
+++ droneCamera.py
@@ -5,15 +5,15 @@ from time import sleep
 from gpiozero import Button # Import Button library
 
 button = Button(17) # set variable 'button' to be at pin 17
-# camera = PiCamera() #initializes the camera variable
+camera = PiCamera() #initializes the camera variable
 
-# camera.start_preview()  
+camera.start_preview()  
 frame = 1   #I use this later to number my pictures
 light = LED(12)
 button.wait_for_press()
 while True:             #This while loop waitsfor the button tobe pressed and t$
     try:                    #It names it based on the variable 'frame' which go$
- #        camera.capture(f"/home/pi/Documents/Engineering_4_Notebook/Pi_Pictures/droneStuff/{frame}.jpg")
+        camera.capture(f"/home/pi/Documents/Engineering_4_Notebook/Pi_Pictures/droneStuff/{frame}.jpg")
         frame += 1
         light.on()
         print("taking")
